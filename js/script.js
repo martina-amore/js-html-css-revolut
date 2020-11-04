2b9caa66d436869b779cada2ded5d2377108dbbc
Martina Amore <martina.amore02@gmail.com>
2020-11-04 18:51:52 +0100

    Risolto piccolo bug js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(".dropdown-link a").click(function(){
+$("a.dropdown").click(function(){
     $(".dropdown-menu").toggleClass("active");
     $(this).parent().siblings().children().removeClass("active");
 })
